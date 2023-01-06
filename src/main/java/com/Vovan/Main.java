da63f37ba7cdf2fae729fc700a3a42406d617ed0
VovanOzz <120048518+Vovanozz@users.noreply.github.com>
2023-01-06 10:16:28 +0200

    vovan

--- src/main/java/com/Vovan/Main.java
+++ src/main/java/com/Vovan/Main.java
@@ -2,6 +2,10 @@ package com.Vovan;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        System.out.println("Hello Vovan!");}
+
+
+
+
+};
+
