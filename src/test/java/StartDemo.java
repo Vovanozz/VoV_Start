533005d7c73242a6cf338e5037921924364ce809
VovanOzz <120048518+Vovanozz@users.noreply.github.com>
2023-02-16 19:18:16 +0200

    1

---
+++ src/test/java/StartDemo.java
@@ -0,0 +1,24 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class StartDemo {
+    WebDriver wd;
+
+    @BeforeMethod
+    public void preCondition(){
+        wd=new ChromeDriver();
+    }
+    @Test
+    public void testLogin(){
+
+    }
+    @AfterMethod
+    public void postCondition(){
+//wd.close();
+    }
+}
