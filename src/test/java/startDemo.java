735c93b65ccb096d03fd0dd6a8f779c48b729e2e
VovanOzz <120048518+Vovanozz@users.noreply.github.com>
2023-02-16 19:38:30 +0200

    111

--- src/test/java/startDemo.java
+++ src/test/java/startDemo.java
@@ -1,4 +1,5 @@
 
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -6,7 +7,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class StartDemo {
+public class startDemo {
     WebDriver wd;
 
     @BeforeMethod
